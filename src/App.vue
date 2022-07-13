e5a101a8e0569d689b1caa2c2b0df9fd05eae594
COEswap <79582960+COEswap@users.noreply.github.com>
2022-07-13 11:11:45 +0200

    v24

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-<PageLoader />
+  <PageLoader />
   <nav>
-    <img class='carmiami' src="./assets/car.png" @click="goHome()" alt="Logo">
+    <img class="carmiami" src="./assets/car.png" @click="goHome()" alt="Logo" />
     <div class="div_nav_2">
       <router-link to="/jeudis">Tous les jeudis</router-link>
       <router-link to="/soon">Prochainement</router-link>
@@ -12,63 +12,82 @@
       <router-link to="/aout">Aout</router-link>
     </div>
   </nav>
-  <router-view/>
+  <router-view />
 
   <body>
-    <div v-if='$route.name == "home"'>
-      <img class="img-fluid une" src="../src/assets/A5.png" @click="goJune()" alt="Programmation 2k22">
+    <div v-if="$route.name == 'home'">
+      <img
+        class="img-fluid une"
+        src="../src/assets/A5.png"
+        @click="goJune()"
+        alt="Programmation 2k22"
+      />
     </div>
-    <div v-if='$route.name == "home"' class="foot">
+    <div v-if="$route.name == 'home'" class="foot">
       <div class="links">
-        <img class="img-fluid insta" src="../src/assets/insta.png" @click="goInsta()" alt="Lien instagram Au Miami">
-        <img class="img-fluid facebook" src="../src/assets/fb.png" @click="goFacebook()" alt="Lien facebook Au Miami">
+        <img
+          class="img-fluid insta"
+          src="../src/assets/insta.png"
+          @click="goInsta()"
+          alt="Lien instagram Au Miami"
+        />
+        <img
+          class="img-fluid facebook"
+          src="../src/assets/fb.png"
+          @click="goFacebook()"
+          alt="Lien facebook Au Miami"
+        />
       </div>
       <div class="network">
-        <img class="img-fluid reseau" src="../src/assets/reseau.png" alt="Au Miami">
+        <img
+          class="img-fluid reseau"
+          src="../src/assets/reseau.png"
+          alt="Au Miami"
+        />
       </div>
     </div>
   </body>
-
 </template>
 
 <script>
-import PageLoader from './components/PageLoader.vue'
+import PageLoader from "./components/PageLoader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    PageLoader
+    PageLoader,
   },
 
   methods: {
-    goHome(){
-      this.$router.push('/');
+    goHome() {
+      this.$router.push("/");
     },
-      goJune(){
-      this.$router.push('/juin');
+    goJune() {
+      this.$router.push("/juin");
     },
-    goInsta(){
-      window.location.href="https://instagram.com/au_miami?igshid=YmMyMTA2M2Y=";
+    goInsta() {
+      window.location.href =
+        "https://instagram.com/au_miami?igshid=YmMyMTA2M2Y=";
     },
-    goFacebook(){
-      window.location.href="https://www.facebook.com/profile.php?id=100063541659991";
+    goFacebook() {
+      window.location.href =
+        "https://www.facebook.com/profile.php?id=100063541659991";
     },
-  }  
-}
+  },
+};
 </script>
 
 <style lang="scss">
-// Desktop 
+// Desktop
 @font-face {
   font-family: "Golden";
   src: local("Golden"), url("./fonts/Golden Ranger.ttf") format("truetype");
 }
 
-body{
+body {
   background-color: #154285;
 }
-
 
 #app {
   -webkit-font-smoothing: antialiased;
@@ -78,25 +97,24 @@ body{
   background-color: #154285;
 }
 
-
 nav {
   padding: 20px;
-  width:100%;
+  width: 100%;
   max-width: 100%;
   position: absolute;
-  display:flex;
+  display: flex;
   justify-content: space-evenly;
 
-  .carmiami{
-    width:15rem;
+  .carmiami {
+    width: 15rem;
   }
-   .div_nav_1{
+  .div_nav_1 {
     position: absolute;
-    display:flex;
+    display: flex;
     justify-content: space-evenly;
-    width:80%;
-    left:10%;
-    top:82%;
+    width: 80%;
+    left: 10%;
+    top: 82%;
 
     a {
       font-weight: bold;
@@ -105,17 +123,17 @@ nav {
       text-decoration: none;
       color: white;
 
-    &.router-link-exact-active {
-      color: #e50b7e;
+      &.router-link-exact-active {
+        color: #e50b7e;
+      }
     }
   }
-  }
-    .div_nav_2{
+  .div_nav_2 {
     position: absolute;
-    display:flex;
+    display: flex;
     justify-content: space-evenly;
-    width:50%;
-    top:140%;
+    width: 50%;
+    top: 140%;
 
     a {
       font-weight: bold;
@@ -124,322 +142,316 @@ nav {
       text-decoration: none;
       color: white;
 
-    &.router-link-exact-active {
-      color: #e50b7e;
+      &.router-link-exact-active {
+        color: #e50b7e;
+      }
     }
-  }
   }
 }
 
-.une{
-  position:relative;
-  top:275px;
-  width:30%;
-  max-width:30%;
+.une {
+  position: relative;
+  top: 275px;
+  width: 30%;
+  max-width: 30%;
   margin-bottom: -100px;
   border: 3px solid white;
   border-radius: 10px;
 }
 
-.foot{
-  position:relative;
-  top:390px;
-  max-width:35%;
-  left:33%;
-  display:flex;
+.foot {
+  position: relative;
+  top: 390px;
+  max-width: 35%;
+  left: 33%;
+  display: flex;
   flex-direction: column;
 
-  .links{
+  .links {
     display: flex;
     justify-content: center;
 
-    .insta{
-      width:125px;
-      margin-left:50px;
+    .insta {
+      width: 125px;
+      margin-left: 50px;
       margin-right: -30px;
     }
-    .facebook{
-      width:125px;
-      height:30%;
+    .facebook {
+      width: 125px;
+      height: 30%;
       padding-top: 0.7%;
       margin-right: 50px;
     }
   }
-  .network{
-    .reseau{
-    width:340px;
-    margin-right: 30px;
+  .network {
+    .reseau {
+      width: 340px;
+      margin-right: 30px;
     }
   }
 }
 
 // Media 949 px
-@media only screen and (max-width:950px) {
+@media only screen and (max-width: 950px) {
   nav {
-  padding: 20px;
-  width:100%;
-  max-width: 100%;
-  position: absolute;
-  display:flex;
-  justify-content: space-evenly;
-
-  .carmiami{
-    width:15rem;
-  }
-   .div_nav_1{
+    padding: 20px;
+    width: 100%;
+    max-width: 100%;
     position: absolute;
-    display:flex;
-    justify-content: space-evenly;
-    width:80%;
-    left:10%;
-    top:82%;
-
-    a {
-      font-weight: bold;
-      font-family: "Golden";
-      font-size: 52px;
-      text-decoration: none;
-      color: white;
-
-    &.router-link-exact-active {
-      color: #e50b7e;
-    }
-  }
-  }
-    .div_nav_2{
-    position: absolute;
-    display:flex;
-    justify-content: space-evenly;
-    width:50%;
-    top:140%;
-
-    a {
-      font-weight: bold;
-      font-family: "Golden";
-      font-size: 32px;
-      text-decoration: none;
-      color: white;
-
-    &.router-link-exact-active {
-      color: #e50b7e;
-    }
-  }
-  }
-}
-}
-
-@media only screen and (max-width:768px) {
-@font-face {
-  font-family: "Golden";
-  src: local("Golden"), url("./fonts/Golden Ranger.ttf") format("truetype");
-}
-
-
-body{
-  background: #154285;
-}
-
-
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: #154285;
-}
-
-nav {
-  padding: 20px;
-  width:100%;
-  max-width: 100%;
-  position: absolute;
-  display:flex;
-  justify-content: space-evenly;
-
-  .carmiami{
-    width:15rem;
-  }
-   .div_nav_1{
-    position: absolute;
-    display:flex;
-    justify-content: space-evenly;
-    width:80%;
-    left:10%;
-    top:82%;
-
-    a {
-      font-weight: bold;
-      font-family: "Golden";
-      font-size: 32px;
-      text-decoration: none;
-      color: white;
-
-    &.router-link-exact-active {
-      color: #e50b7e;
-    }
-  }
-  }
-    .div_nav_2{
-    position: absolute;
-    display:flex;
-    justify-content: space-evenly;
-    width:60%;
-    top:120%;
-
-    a {
-      font-weight: bold;
-      font-family: "Golden";
-      font-size: 22px;
-      text-decoration: none;
-      color: white;
-
-    &.router-link-exact-active {
-      color: #e50b7e;
-    }
-  }
-  }
-}
-
-
-.une{
-  position:relative;
-  top:215px;
-  width:80%;
-  max-width:80%;
-  margin-bottom: -100px;
-   border: 3px solid white;
-   border-radius: 10px;
-}
-
-.foot{
-  position:relative;
-  top:320px;
-  max-width:60%;
-  display:flex;
-  left:19%;
-  flex-direction: column;
-
-  .links{
     display: flex;
-    justify-content: center;
-    align-items: center;
-
-    .insta{
-      width:80px;
-      margin-left:40px;
-      margin-right: 1px;
-    }
-    .facebook{
-      width:80px;
-      height:30%;
-      padding-top: 0.1%;
-      margin-right: 30px;
-    }
-  }
-  .network{
-    .reseau{
-    width:340px;
-    }
-  }
-}
-
-  
-}
-
-@media only screen and (max-width:461px) {
-nav {
-  padding: 20px;
-  width:100%;
-  max-width: 100%;
-  position: absolute;
-  display:flex;
-  justify-content: space-evenly;
-
-  .carmiami{
-    width:15rem;
-  }
-   .div_nav_1{
-    position: absolute;
-    display:flex;
     justify-content: space-evenly;
-    width:80%;
-    left:10%;
-    top:82%;
 
-    a {
-      font-weight: bold;
-      font-family: "Golden";
-      font-size: 32px;
-      text-decoration: none;
-      color: white;
+    .carmiami {
+      width: 15rem;
+    }
+    .div_nav_1 {
+      position: absolute;
+      display: flex;
+      justify-content: space-evenly;
+      width: 80%;
+      left: 10%;
+      top: 82%;
 
-    &.router-link-exact-active {
-      color: #e50b7e;
+      a {
+        font-weight: bold;
+        font-family: "Golden";
+        font-size: 52px;
+        text-decoration: none;
+        color: white;
+
+        &.router-link-exact-active {
+          color: #e50b7e;
+        }
+      }
+    }
+    .div_nav_2 {
+      position: absolute;
+      display: flex;
+      justify-content: space-evenly;
+      width: 50%;
+      top: 140%;
+
+      a {
+        font-weight: bold;
+        font-family: "Golden";
+        font-size: 32px;
+        text-decoration: none;
+        color: white;
+
+        &.router-link-exact-active {
+          color: #e50b7e;
+        }
+      }
     }
   }
+}
+
+@media only screen and (max-width: 768px) {
+  @font-face {
+    font-family: "Golden";
+    src: local("Golden"), url("./fonts/Golden Ranger.ttf") format("truetype");
   }
-    .div_nav_2{
+
+  body {
+    background: #154285;
+  }
+
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background-color: #154285;
+  }
+
+  nav {
+    padding: 20px;
+    width: 100%;
+    max-width: 100%;
     position: absolute;
-    display:flex;
-    justify-content: space-evenly;
-    width:100%;
-    top:120%;
-
-    a {
-      font-weight: bold;
-      font-family: "Golden";
-      font-size: 22px;
-      text-decoration: none;
-      color: white;
-
-    &.router-link-exact-active {
-      color: #e50b7e;
-    }
-  }
-  }
-}
-
-.une{
-  position:relative;
-  top:215px;
-  width:80%;
-  max-width:80%;
-  margin-bottom: -100px;
-   border: 3px solid white;
-   border-radius: 10px;
-}
-
-.foot{
-  position:relative;
-  top:320px;
-  max-width:60%;
-  display:flex;
-  left:19%;
-  flex-direction: column;
-
-  .links{
     display: flex;
-    justify-content: center;
-    align-items: center;
+    justify-content: space-evenly;
 
-    .insta{
-      width:80px;
-      margin-left:40px;
-      margin-right: 1px;
+    .carmiami {
+      width: 15rem;
     }
-    .facebook{
-      width:80px;
-      height:30%;
-      padding-top: 0.1%;
-      margin-right: 30px;
+    .div_nav_1 {
+      position: absolute;
+      display: flex;
+      justify-content: space-evenly;
+      width: 80%;
+      left: 10%;
+      top: 82%;
+
+      a {
+        font-weight: bold;
+        font-family: "Golden";
+        font-size: 32px;
+        text-decoration: none;
+        color: white;
+
+        &.router-link-exact-active {
+          color: #e50b7e;
+        }
+      }
+    }
+    .div_nav_2 {
+      position: absolute;
+      display: flex;
+      justify-content: space-evenly;
+      width: 60%;
+      top: 120%;
+
+      a {
+        font-weight: bold;
+        font-family: "Golden";
+        font-size: 22px;
+        text-decoration: none;
+        color: white;
+
+        &.router-link-exact-active {
+          color: #e50b7e;
+        }
+      }
     }
   }
-  .network{
-    .reseau{
-    width:340px;
+
+  .une {
+    position: relative;
+    top: 215px;
+    width: 80%;
+    max-width: 80%;
+    margin-bottom: -100px;
+    border: 3px solid white;
+    border-radius: 10px;
+  }
+
+  .foot {
+    position: relative;
+    top: 320px;
+    max-width: 60%;
+    display: flex;
+    left: 19%;
+    flex-direction: column;
+
+    .links {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .insta {
+        width: 80px;
+        margin-left: 40px;
+        margin-right: 1px;
+      }
+      .facebook {
+        width: 80px;
+        height: 30%;
+        padding-top: 0.1%;
+        margin-right: 30px;
+      }
+    }
+    .network {
+      .reseau {
+        width: 340px;
+      }
     }
   }
 }
-}
 
+@media only screen and (max-width: 461px) {
+  nav {
+    padding: 20px;
+    width: 100%;
+    max-width: 100%;
+    position: absolute;
+    display: flex;
+    justify-content: space-evenly;
+
+    .carmiami {
+      width: 15rem;
+    }
+    .div_nav_1 {
+      position: absolute;
+      display: flex;
+      justify-content: space-evenly;
+      width: 80%;
+      left: 10%;
+      top: 82%;
+
+      a {
+        font-weight: bold;
+        font-family: "Golden";
+        font-size: 32px;
+        text-decoration: none;
+        color: white;
+
+        &.router-link-exact-active {
+          color: #e50b7e;
+        }
+      }
+    }
+    .div_nav_2 {
+      position: absolute;
+      display: flex;
+      justify-content: space-evenly;
+      width: 100%;
+      top: 120%;
+
+      a {
+        font-weight: bold;
+        font-family: "Golden";
+        font-size: 22px;
+        text-decoration: none;
+        color: white;
+
+        &.router-link-exact-active {
+          color: #e50b7e;
+        }
+      }
+    }
+  }
+
+  .une {
+    position: relative;
+    top: 215px;
+    width: 80%;
+    max-width: 80%;
+    margin-bottom: -100px;
+    border: 3px solid white;
+    border-radius: 10px;
+  }
+
+  .foot {
+    position: relative;
+    top: 320px;
+    max-width: 60%;
+    display: flex;
+    left: 19%;
+    flex-direction: column;
+
+    .links {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .insta {
+        width: 80px;
+        margin-left: 40px;
+        margin-right: 1px;
+      }
+      .facebook {
+        width: 80px;
+        height: 30%;
+        padding-top: 0.1%;
+        margin-right: 30px;
+      }
+    }
+    .network {
+      .reseau {
+        width: 340px;
+      }
+    }
+  }
+}
 </style>
--- src/views/AsAp.vue
+++ src/views/AsAp.vue
@@ -1,94 +1,104 @@
 <template>
-    <img class="img-fluid comsoon" src="../assets/07-07.jpeg" alt="Prochainement">
-    <img class="img-fluid comsoon1" src="../assets/09__07.jpeg" alt="Prochainement">
+  <img
+    class="img-fluid comsoon"
+    src="../assets/14_07.jpeg"
+    alt="Prochainement"
+  />
+  <img
+    class="img-fluid comsoon1"
+    src="../assets/15_07.jpeg"
+    alt="Prochainement"
+  />
+  <img
+    class="img-fluid comsoon2"
+    src="../assets/16_07.jpeg"
+    alt="Prochainement"
+  />
 </template>
 
 <script>
 export default {
   // eslint-disable-next-line
-  name: 'Home',
-
-}
+  name: "Home",
+};
 </script>
 
 
 <style lang="scss" scoped>
-
-.comsoon{
-  position:relative;
-  top:275px;
-  width:80%;
-  max-width:80%;
+.comsoon {
+  position: relative;
+  top: 275px;
+  width: 80%;
+  max-width: 80%;
   margin-bottom: -30%;
-   border: 3px solid white;
-   border-radius: 10px;
+  border: 3px solid white;
+  border-radius: 10px;
 }
-.comsoon1{
-  position:relative;
-  top:735px;
-  width:80%;
-  max-width:80%;
+.comsoon1 {
+  position: relative;
+  top: 735px;
+  width: 80%;
+  max-width: 80%;
   margin-bottom: -30%;
-   border: 3px solid white;
-   border-radius: 10px;
+  border: 3px solid white;
+  border-radius: 10px;
 }
-.comsoon2{
-  position:relative;
-  top:1195px;
-  width:80%;
-  max-width:80%;
+.comsoon2 {
+  position: relative;
+  top: 1195px;
+  width: 80%;
+  max-width: 80%;
   margin-bottom: -30%;
-   border: 3px solid white;
-   border-radius: 10px;
+  border: 3px solid white;
+  border-radius: 10px;
 }
-.comsoon3{
-  position:relative;
-  top:1655px;
-  width:80%;
-  max-width:80%;
+.comsoon3 {
+  position: relative;
+  top: 1655px;
+  width: 80%;
+  max-width: 80%;
   margin-bottom: -30%;
-   border: 3px solid white;
-   border-radius: 10px;
+  border: 3px solid white;
+  border-radius: 10px;
 }
-@media only screen and (max-width:767px) {
-  .comsoon{
-  position:relative;
-  top:215px;
-  width:80%;
-  max-width:80%;
-  margin-bottom: -30%;
-   border: 3px solid white;
-   border-radius: 10px;
+@media only screen and (max-width: 767px) {
+  .comsoon {
+    position: relative;
+    top: 215px;
+    width: 80%;
+    max-width: 80%;
+    margin-bottom: -30%;
+    border: 3px solid white;
+    border-radius: 10px;
+  }
+  .comsoon1 {
+    position: relative;
+    top: 375px;
+    width: 80%;
+    max-width: 80%;
+    margin-bottom: -30%;
+    border: 3px solid white;
+    border-radius: 10px;
+  }
+  .comsoon2 {
+    position: relative;
+    top: 535px;
+    width: 80%;
+    max-width: 80%;
+    margin-bottom: -30%;
+    border: 2px solid white;
+    border-radius: 10px;
+    z-index: 1;
+  }
+  .comsoon3 {
+    position: relative;
+    top: 695px;
+    width: 80%;
+    max-width: 80%;
+    margin-bottom: -30%;
+    border: 2px solid white;
+    border-radius: 10px;
+    z-index: 1;
+  }
 }
-.comsoon1{
-  position:relative;
-  top:375px;
-  width:80%;
-  max-width:80%;
-  margin-bottom: -30%;
-   border: 3px solid white;
-   border-radius: 10px;
-}
-.comsoon2{
-  position:relative;
-  top:535px;
-  width:80%;
-  max-width:80%;
-  margin-bottom: -30%;
-   border: 2px solid white;
-   border-radius: 10px;
-   z-index: 1;
-}
-.comsoon3{
-  position:relative;
-  top:695px;
-  width:80%;
-  max-width:80%;
-  margin-bottom: -30%;
-   border: 2px solid white;
-   border-radius: 10px;
-   z-index: 1;
-}
-}
-
 </style>
